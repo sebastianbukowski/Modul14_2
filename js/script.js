81429312b70bc5ea7d8d936e325eb8f76ba48e63
sebastianbukowski <seb-27@o2.pl>
2019-01-29 18:35:37 +0100

    Add picture to description of films

---
+++ js/script.js
@@ -0,0 +1,50 @@
+
+ var movies = [
+    {
+      id: 1,
+      title: 'Harry Potter',
+      desc: 'film o czarodzieju',
+      image: '../images/harry_potter.jpg'
+    },
+    {
+      id: 2,
+      title: 'Król Lew',
+      desc: 'Film o królu sawanny',
+      image: '../images/krol_lew.jpg'
+    },
+    {
+     id: 3,
+     title: 'Mumia',
+     desc: 'Komandos marynarki wojennej, Nick Morton, wyrusza z zespołem do Iraku, by odnaleźć ukrywającą         się w bunkrze grupę terrorystów. Na miejscu odkrywa starożytny grobowiec, gdzie czyha ukryte          niebezpieczeństwo.',
+     image: '../images/mumia.jpg'
+      },
+      {
+     id: 4,
+     title: 'Avatar',
+     desc: 'Jake, sparaliżowany były komandos, zostaje wysłany na planetę Pandora, gdzie zaprzyjaźnia się        z lokalną społecznością i postanawia jej pomóc. ',
+     image: '../images/avatar.jpg'
+      },
+      {
+     id: 5,
+     title: 'Książę Persji: Piaski czasu',
+     desc: 'Dastan, przybrany syn króla Persji, zostaje wrobiony w morderstwo swojego ojca. Wraz z               niepokorną księżniczką Taminą wyrusza w podróż, aby dowieść swojej niewinności.',
+     image: '../images/ksiaze_persji.jpg'
+      }
+  ];
+  var moviesElements = movies.map(function(movie) {
+    return React.createElement('li', {key: movie.id},
+        React.createElement('h2', {}, movie.title),
+        React.createElement('p', {}, movie.desc),
+        React.createElement('img', {src: movie.image})
+      );
+  });
+  var element =
+    React.createElement('div', {},
+    React.createElement('h1', {}, 'Lista filmów'),
+    React.createElement('ul', {}, moviesElements),
+);
+
+
+  ReactDOM.render(element, document.getElementById('app')); 
+
+ 
